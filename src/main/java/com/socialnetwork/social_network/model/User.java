594f13680e8e9fad2f5b554dec3e6d2709041f8d
Nick <nkuuee@bk.ru>
2025-03-07 22:24:04 +0300

    first

---
+++ src/main/java/com/socialnetwork/social_network/model/User.java
@@ -0,0 +1,34 @@
+package com.socialnetwork.social_network.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime registrationData = LocalDateTime.now();
+
+}
